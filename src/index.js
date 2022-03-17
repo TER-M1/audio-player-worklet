31a583287d0cb44ac855ff1346856b43f87eab17
MonsieurCo <monsieur.cousson@gmail.com>
2022-03-17 15:31:04 +0100

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 //@ts-check
 
-const audioUrl = "https://wasabi.i3s.unice.fr/WebAudioPluginBank/BasketCaseGreendayriffDI.mp3";
-
+// const audioUrl = "https://wasabi.i3s.unice.fr/WebAudioPluginBank/BasketCaseGreendayriffDI.mp3";
+const audioUrl = ""
 const audioCtx = new AudioContext();
 const gainNode = audioCtx.createGain();
 /** @type {HTMLButtonElement} */
 // @ts-ignore
 const btnStart = document.getElementById("btn-start");
+/** @type {HTMLButtonElement} */
+// @ts-ignore
+const zoomIn = document.getElementById("btn-zoom-in");
+/** @type {HTMLButtonElement} */
+// @ts-ignore
+const zoomOut = document.getElementById("btn-zoom-out");
+
+var zoom = 1;
+
 /** @type {HTMLInputElement} */
 // @ts-ignore
 const inputLoop = document.getElementById("btn-check-2-outlined");
@@ -24,6 +33,7 @@ var paused = false;
 
 function drawBuffer (canvas, buffer, color,width,height) {
     var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     canvas.width = width ;
     canvas.height = height;
     if (color) {
@@ -33,10 +43,10 @@ function drawBuffer (canvas, buffer, color,width,height) {
       var data = buffer.getChannelData( 0 );
       var step = Math.ceil( data.length / width );
       var amp = height / 2;
-      for(var i=0; i < width; i++){
-          var min = 1.0;
+      for(var i=0; i < width; i ++){
+          var min =  1.0;
           var max = -1.0;
-          for (var j=0; j<step; j++) {
+          for (var j=0; j<step * zoom; j++) {
               var datum = data[(i*step)+j];
               if (datum < min)
                   min = datum;
@@ -141,7 +151,15 @@ function muteUnmuteTrack(btn){
     node.parameters.get("playing").value = 0;
     node.parameters.get("loop").value = 1;
     
-
+    zoomIn.onclick = () => {
+        zoom += 1000;
+        drawBuffer(canvas0,audioBuffer,'red',1000,300);
+    }
+    zoomOut.onclick = () => {
+        zoom -= 1000;
+        zoom = zoom < 0 ? 1 : zoom;
+        drawBuffer(canvas0,audioBuffer,'red',1000,300);
+    }
     btnStart.onclick = () => {
         if (audioCtx.state === "suspended"){ 
             audioCtx.resume();
